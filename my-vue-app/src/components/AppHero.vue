b18104096b2ac063a5bf9d27eece83ceb27d6f7e
paoloevan <paoloev1900@gmail.com>
2022-11-09 15:04:35 +0100

    add image

--- my-vue-app/src/components/AppHero.vue
+++ my-vue-app/src/components/AppHero.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row py-2">
-        <div class="h-100 p-5 text-white bg-primary border rounded-3 text-center ">
+        <div class="jumbo h-100 p-5 text-white border rounded-3 text-center ">
             <h2 class="pb-2">Il mio primo Jumbotron con Vite</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id non voluptate nam quae eveniet iste ducimus
                 consequatur nisi mollitia quas! Ipsa, tenetur vero incidunt esse adipisci dicta quia eos magni.</p>
@@ -9,5 +9,8 @@
 </template>
 
 <style scoped>
-
+.jumbo {
+    background-image: url(../../public/1618197067vitejs.webp);
+    background-position: center;
+}
 </style>
