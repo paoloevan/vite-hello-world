2feada4eb649aba7be6f11c2c9760bc629571edc
paoloevan <paoloev1900@gmail.com>
2022-11-09 14:51:16 +0100

    add bonus

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,16 +1,22 @@
 <script>
 
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue'
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   }
 }
 </script>
 
 <template>
+
   <AppTitle />
+
+  <AppHero />
+
 </template>
 
 <style>
---
+++ my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row py-2">
+        <div class="h-100 p-5 text-white bg-primary border rounded-3 text-center ">
+            <h2 class="pb-2">Il mio primo Jumbotron con Vite</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id non voluptate nam quae eveniet iste ducimus
+                consequatur nisi mollitia quas! Ipsa, tenetur vero incidunt esse adipisci dicta quia eos magni.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
